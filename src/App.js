79a0fae5478181b5470e26f3c174876ee4ceb43b
Ermishina Evgeniya <evermishka@yandex.ru>
2024-08-25 11:07:01 +0300

    rewrite component without jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-// Императивный
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 
-export const App = () => {
-	return (
-		// Декларативный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					{new Date().getFullYear()}
-				</p>
-			</header>
-		</div>
+export const App = () => {	
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement(
+				'img',
+				{ 
+					className: 'App-logo', 
+					src: logo, 
+					alt: 'logo' 
+				}
+			),
+			createElement(
+				'p',
+				{},
+				'Edit ',
+				createElement(
+					'code',
+					{},
+					'src/App.js'
+				),				
+				' and save to reload.'
+			),
+			createElement(
+				'a',
+				{ 
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer'
+				},
+				'Learn React'
+			),
+			createElement(
+				'p',
+				{},
+				`${new Date().getFullYear()}`
+			)
+		)
 	);
 };
